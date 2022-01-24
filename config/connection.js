219cf7adb1108c5fb14c9d87887f8f306bcbeb03
KDash201 <KVonMadison@Yahoo.com>
2022-01-23 23:10:25 -0500

    captured first input

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,11 @@
-const Sequelize = require('sequelize');
+const mysql = require("mysql2");
+require("dotenv").config();
 
-require('dotenv').config();
+// Connect to database
+const db = mysql.createConnection({
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+});
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
-
-module.exports = sequelize;
+module.exports = db;
--- server.js
+++ server.js
@@ -1,2 +1,70 @@
+const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const db = require('./config/connection');
+const db = require("./config/connection");
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected to Employee Database');
+    introPrompt();
+});
+
+const introPrompt = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "start",
+      message: "Please make a selection",
+      choices: [
+        "view departments",
+        "view roles",
+        "view employees",
+        "add department",
+        "add role",
+        "add  employee",
+        "update  employee role",
+        "quit",
+      ],
+    })
+    .then((answers) => {
+      switch (answers.start) {
+        case "view departments":
+          viewDepartments();
+          break;
+        case "view roles":
+          viewRoles();
+          break;
+        case "view employees":
+          viewEmployees();
+          break;
+        case "add department":
+          addDepartment();
+          break;
+        case "add role":
+          addRole();
+          break;
+        case "add employee":
+          addEmployee();
+          break;
+        case "update employee role":
+          updateRole();
+          break;
+        case "quit":
+          console.log("Thank you! Good bye!");
+          process.exit(0);
+      }
+    });
+};
+
+
+db.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM departments", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
+  
+
+
+introPrompt();
+
